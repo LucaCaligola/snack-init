29831581c998b555756f71c9f2f6c33c35e78f00
LucaCaligola <luca.caligola@gmail.com>
2024-01-30 09:43:34 +0100

    a new index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<!-- Parte 1:
+Creare un nuovo oggetto Persona con un metodo setAge() che cambia il valore della proprieta' age della classe Persona, 
+che accetti esclusivamente un numero intero. 
+Se il numero inserito come argomento non dovesse essere un numero intero: allora lanciare un'eccezione! -->
